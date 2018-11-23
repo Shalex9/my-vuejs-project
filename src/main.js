52c19e1f99eb76e18df827dcd590be31a0ef3a6e
shalex <shalex9@gmail.com>
2018-11-23 13:19:44 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ import App from './App.vue'
 import router from './router'
 // import VueRouter from './vue-router'
 import VueResource from 'vue-resource'
-import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+// import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vue-material/dist/vue-material.min.css'
 
 import VueMaterial from 'vue-material'
 import { MdToolbar, MdButton, MdList, MdTabs, MdField, MdSwitch, MdCard, MdRipple, MdProgress } from 'vue-material/dist/components'
-import Vuetify from 'vuetify'
+// import Vuetify from 'vuetify'
 // import MaterialKit from './plugins/material-kit'
 
-Vue.use(Vuetify)
+// Vue.use(Vuetify)
 // Vue.use(MaterialKit)
 // Vue.use(VueRouter)
 Vue.use(VueMaterial)
